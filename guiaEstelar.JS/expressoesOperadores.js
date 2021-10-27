73139fd6b1613e30192a2e7fe9a234756e9ae0dc
beasou <beatrizreginasouza1@gmail.com>
2021-10-27 18:32:18 -0300

    guiaEstelar.js Expressoes e Operadores(Binary, unary, ternary)

---
+++ guiaEstelar.JS/expressoesOperadores.js
@@ -0,0 +1,45 @@
+/*
+    EXPRESSÕES E OPERADORES -->
+    Expresssões são bem tranquilas, porque expressões são qualquer linha de código que resolve alguma coisa no JavaScript, um exemplo é a expressão:
+    let number
+    sso é uma expressão, já que estamos declarando uma váriavel, nesse caso com o valor de undefined.
+
+    Toda expressão do Javascript pode ou não terminar com ; (ponto e vírgula), o final de uma expressão é facultativa, mas em algumas outras linguagens é obrigatório o uso de ; (ponto e vírgula).
+
+    Em alguns casos é necessário o uso de ; (ponto e vírgula), como numa função auto-executável, é um dos poucos casos onde faz falta, e é em um cenário como o seguinte:
+    let number = 1
+
+    (function(){
+      console.log('alo')
+    )()
+    
+    -Expressions
+    -Operadores
+        Binary
+        Unary
+        Ternary
+
+    
+*/
+    let num = 1; // sem essa virgula ele entende como nome da função, então nesse caso é melhor colocar uma virgula
+
+    (function(){
+    console.log('alo')
+    })()
+
+//Caso você tente executar esse código, ele vai tentar atribuir essa função ao valor 1, onde o Javascript vai tentar executar o 1, passar uma função como argumento, e o 1 vai receber uma outra função. Um dos pouquíssimos casos onde o ; (ponto e vírgula) faz falta.
+
+//Veremos também sobre operadores, nesse exemplo temos o binário, unário e ternário.
+
+    /* unário */
+    let num1 = 1
+    console.log(++num1)//incremendo ou decremento "--"
+
+    /* binário */
+    let num2 = 1
+    console.log(num2 + 4)
+
+    /* ternário */
+    console.log(false ? 'alo' : 'nada')
+
+//Como você pode observar, temos no unário um elemento, no binário dois elementos e no ternário três elementos.
