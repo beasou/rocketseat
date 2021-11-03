8dafa858a697e31e2f228c5508bf70eb35f413e6
beasou <beatrizreginasouza1@gmail.com>
2021-11-02 21:59:53 -0300

    guiaEstelar.js Expressoes e Operadores(Operadores de comparaçao maior e menor (igual))

--- guiaEstelar.JS/expressoesOperadores.js
+++ guiaEstelar.JS/expressoesOperadores.js
@@ -138,3 +138,32 @@
     console.log(one != two)
     console.log(one != 1)
     console.log(one != "1")
+
+    // === estritamente igual a 
+    console.log(one === "1")
+    console.log(one === 1) 
+
+    // !== estritamente diferente de
+    console.log(two !== "2")
+    console.log(two !== 2)
+
+//----------------------------------------------------------------
+/*        
+    Operadores de comparação maior e menor (igual)
+*/
+
+    // > maior que
+    console.log(one > two)
+
+    // >= maior igual a
+    console.log(one >= 1)
+    console.log(two >= 1)
+
+    // < menor que 
+    console.log(one < two)
+
+    // <= menor igual a
+    console.log(one <= two)
+    console.log(one <= 1)
+    console.log(one <= 0)
+
