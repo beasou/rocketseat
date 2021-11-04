0700acfb9e2beec5cd8e21010b537c4ddf99cb96
beasou <beatrizreginasouza1@gmail.com>
2021-11-03 21:31:50 -0300

    guiaEstelar.js Condicionais e controle de fluxo(switch)

--- guiaEstelar.JS/condicionaisControleFluxo.js
+++ guiaEstelar.JS/condicionaisControleFluxo.js
@@ -18,3 +18,51 @@
     }
 
 //--------------------------------------------------------
+/* 
+    SWITCH
+*/
+    let expression = ""
+    switch(expression){
+        case "a":
+            //codigo para expressão a
+            console.log("a")
+            break
+        case "b":
+            //expressão para expressão b
+            console.log("b")
+            break
+        default:
+            console.log("default")
+            break
+    }
+
+    //exemplo calculadora 
+    function calculate(numb1, operator, numb2){
+        let result
+
+        switch(operator){
+            case '+':
+                result = numb1 + numb2
+                break
+            case '-':
+                result = numb1 - numb2
+                break
+            case '*':
+                result = numb1 * numb2
+                break
+            case '/':
+                result = numb1 / numb2
+                break
+            default:
+                console.log("não implementado")
+                break
+        }
+        return result
+    }
+    console.log(calculate(2,"+",2))
+    console.log(calculate(2,"-",2))
+    console.log(calculate(2,"*",2))
+    console.log(calculate(2,"/",2))
+    console.log(calculate(2,"%",2))
+    
+//--------------------------------------------------------
