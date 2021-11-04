986343447bec409adfd8857c570debe6c674db5f
beasou <beatrizreginasouza1@gmail.com>
2021-11-03 21:52:06 -0300

    guiaEstelar.js Estruturas de Repetiçao(For)

---
+++ guiaEstelar.JS/estruturasRepeticao.js
@@ -0,0 +1,17 @@
+/* 
+    FOR
+*/
+    for(let i = 10; i >= 0; i--) {
+         console.log(i)
+    }
+
+    for(let i = 0; i <= 10; i++) {
+        if(i === 5){
+            break; //para execução
+            continue; //pula execução no momento
+         }
+         console.log(i)
+    }
+
+    
+//--------------------------------------------------------
