a2554fd44b1a319d063f5f7e3d368d9298093293
beasou <beatrizreginasouza1@gmail.com>
2022-01-04 10:54:48 -0300

    Pilotando a DOM - Selecionando elementos(getElementsByClassName() / getElementsByTagName )

--- pilotandoComADOM/selecionandoElementos.js
+++ pilotandoComADOM/selecionandoElementos.js
@@ -1,3 +1,11 @@
 //getElementById()
 const element = document.getElementById('blog-title')
 console.log(element)
+
+//getElementsByClassName()
+const element = document.getElementsByClassName('blog-text')
+console.log(element)
+
+//getElementsByTagName()
+const element = document.getElementsByTagName('meta')
+console.log(element)
