3cdaa82733a08c2d57716496dfcb27618f1ef1d0
beasou <beatrizreginasouza1@gmail.com>
2022-01-05 20:53:13 -0300

    insertBefore

--- pilotandoComADOM/criandoAdicionandoElementosPagina.js
+++ pilotandoComADOM/criandoAdicionandoElementosPagina.js
@@ -6,3 +6,8 @@ div.innerText = "adicionando aqui" //criando o texto da div para ser add
 const bod = document.querySelector('body')
 bod.append(div) //add depois    
 bod.prepend(div) //add antes do elemento, no caso body
+
+
+//insertBefore
+const script = bod.querySelector('script')
+bod.insertBefore(div,script)//1ºqual é o elemento novo, 2º no de referencia add antes da tag script
