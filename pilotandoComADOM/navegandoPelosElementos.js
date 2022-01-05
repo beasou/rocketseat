d35f7ff99a991b3f257af5e568f15a3ff332c95f
beasou <beatrizreginasouza1@gmail.com>
2022-01-05 20:29:27 -0300

    Pegando elementos filhos- childNodes, children, firstElementChild, lastElementChild

--- pilotandoComADOM/navegandoPelosElementos.js
+++ pilotandoComADOM/navegandoPelosElementos.js
@@ -1,4 +1,14 @@
 //Navegando pelos elementos
-//parentNode parentElement
+//parentNode parentElement (pais)
 const body = document.querySelector('body')
 console.log(body.parentNode)
+
+//Pegando elementos filhos
+//childNodes children
+const el = document.querySelector('body')
+console.log(el.childNodes) //text que aparece são espaços
+console.log(el.children) //sem os espaços o htmlcollection não leva em conta
+//firstChild firstElementChild
+console.log(el.firstElementChild) //firstChild leva em consideração o espaço em vazio e o firstElementChild não.
+//lastChild lastElementChild
+console.log(el.lastElementChild)
