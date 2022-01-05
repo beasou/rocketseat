cfddf1d3c192d213ee4b8bb4bd3a74f5737f1b2a
beasou <beatrizreginasouza1@gmail.com>
2022-01-04 21:27:35 -0300

    Manipulando conteudos - innerText

--- pilotandoComADOM/manipulandoConteudos.js
+++ pilotandoComADOM/manipulandoConteudos.js
@@ -4,3 +4,7 @@
 const elemento = document.querySelector('h1')
 elemento.textContent += " Olá Devs!"//além de modificar podemos concatenar
 console.log(elemento.textContent)
+
+//innerText
+elemento.innerText = " Olá Devs!"
+console.log(elemento.innerText)
