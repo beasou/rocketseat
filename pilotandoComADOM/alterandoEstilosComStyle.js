27a28a11ea0203b9e30205fca785b1d021eaf550
beasou <beatrizreginasouza1@gmail.com>
2022-01-04 21:57:50 -0300

    ClassList

--- pilotandoComADOM/alterandoEstilosComStyle.js
+++ pilotandoComADOM/alterandoEstilosComStyle.js
@@ -1,5 +1,11 @@
 //Alterando estilos
 
 const elemen = document.querySelector('body')
-elemen.style.backgroundColor = "#F9F3D2"
+//elemen.style.backgroundColor = "#F9F3D2"
 console.log(elemen.style.backgroundColor)
+
+//classList
+elemen.classList.add('active', 'green')
+console.log(elemen.classList)
+elemen.classList.remove('active')//remove
+elemen.classList.toggle('active')//pesquisa se classe existir remove, se não add
