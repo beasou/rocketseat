520a42ce1e4cf7010ad14081ef70c70947183dea
beasou <beatrizreginasouza1@gmail.com>
2022-01-04 21:07:41 -0300

    Pilotando a DOM - Selecionando elementos(querySelectorAll())

--- pilotandoComADOM/selecionandoElementos.js
+++ pilotandoComADOM/selecionandoElementos.js
@@ -7,5 +7,14 @@ const element = document.getElementsByClassName('blog-text')
 console.log(element)
 
 //getElementsByTagName()
-const element = document.getElementsByTagName('meta')
+const element = document.getElementsByTagName('body')
 console.log(element)
+
+//querySelector() (pesquisa pelo seletor o primeiro que acha na pag)
+const element = document.querySelector('p')
+console.log(element)
+
+//querySelectorAll (td que achar pelo seletor semelhante ao css)
+const elements = document.querySelectorAll('#blog-text')
+
+elements.forEach(el => console.log(el))
