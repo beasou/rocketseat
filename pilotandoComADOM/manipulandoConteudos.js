09be6e516114387ab3ad252598216425a75f4b54
beasou <beatrizreginasouza1@gmail.com>
2022-01-04 21:25:27 -0300

    Manipulando conteudos - textContent

---
+++ pilotandoComADOM/manipulandoConteudos.js
@@ -0,0 +1,6 @@
+//MANIPULANDO CIONTEÚDO
+// textContent
+
+const elemento = document.querySelector('h1')
+elemento.textContent += " Olá Devs!"//além de modificar podemos concatenar
+console.log(elemento.textContent)
--- pilotandoComADOM/selecionandoElementos.js
+++ pilotandoComADOM/selecionandoElementos.js
@@ -1,4 +1,12 @@
-/*//getElementById()
+/*
+    getElementById (retorna um elemento)
+    getElementsByClassName() (retorno de dado HTMLCollection) -> NÃO conseguimos fazer um forEach
+    getElementsByTagName(retorna um HTMLCollection)
+    querySelector(RETORNA ELEMENTO)
+    querySelectorAll (retorno de dado Nodelist) -> conseguimos fazer um forEach
+*/
+/*
+//getElementById()
 const element = document.getElementById('blog-title')
 console.log(element)
 
@@ -13,8 +21,9 @@ console.log(element)
 //querySelector() (pesquisa pelo seletor o primeiro que acha na pag)
 const element = document.querySelector('p')
 console.log(element)
-*/
+
 //querySelectorAll (td que achar pelo seletor semelhante ao css)
 const elements = document.querySelectorAll('.blog-text')
 
 elements.forEach(el => console.log(el))
+*/
