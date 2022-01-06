b0de1cbae5246de4589bf526d73ac6de201c42f2
beasou <beatrizreginasouza1@gmail.com>
2022-01-05 21:13:17 -0300

    Adicionando eventos via JS

--- pilotandoComADOM/eventos.js
+++ pilotandoComADOM/eventos.js
@@ -11,3 +11,11 @@ const input =document.querySelector('input')
 input.onkeyup = function() { //preciou o botão de seta pra cima vai disparar o alert
     alert('rodei')
 }
+
+//Adicionando eventos via JS
+const h1 = document.querySelector('h1')
+
+h1.addEventListener('mouseover', print)
+function print(){
+    console.log('print')
+}
