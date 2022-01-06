ebe0aef0ae4a36ad89c1e49c94995b95b1e68441
beasou <beatrizreginasouza1@gmail.com>
2022-01-05 21:07:28 -0300

    Eventos de teclado

--- pilotandoComADOM/eventos.js
+++ pilotandoComADOM/eventos.js
@@ -4,3 +4,10 @@
 function print(){
     alert("Aqui")
 }
+
+//eventos de Teclado
+const input =document.querySelector('input')
+
+input.onkeyup = function() { //preciou o botão de seta pra cima vai disparar o alert
+    alert('rodei')
+}
