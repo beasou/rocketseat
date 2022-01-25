530924706b4458708ebd81a925f7863c5bbeeb45
beasou <beatrizreginasouza1@gmail.com>
2022-01-24 22:18:21 -0300

    desafio

---
+++ nodeJS/desafio.js
@@ -0,0 +1,9 @@
+/* Crie 2 arquivos Java Script.
+    1- o primeiro, irá exportar uma função chamada getFlag()
+        que receberá um argumento do tipo String.
+        essa função deverá buscar dentro do array a flag e retornar o valor da flag
+
+        o segundo irá importar a função e passar a flag desejada.
+
+        iremos rodar no terminal o segundo arquivo passando as flags --name e --greeting
+        para que seja impresso no terminar a saudação e o nome da pessoa.*/
