5404944090ebe9c16a992b86c18c7afa59ee3c37
beasou <beatrizreginasouza1@gmail.com>
2022-01-24 22:14:22 -0300

    process

---
+++ nodeJS/process.js
@@ -0,0 +1,6 @@
+//console.log(process.argv) //tras uma lista de argumentos que estou usando no node
+
+const firstName = process.argv[2]
+const lastName = process.argv[3]
+
+console.log(`Seu nome é ${firstName} ${lastName}`)
