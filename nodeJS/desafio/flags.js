bcfd4e7f1501c1bc5453ac9b338f9b78787dfd5e
beasou <beatrizreginasouza1@gmail.com>
2022-02-02 13:07:40 -0300

    Soluçao Desafio

--- nodeJS/desafio/flags.js
+++ nodeJS/desafio/flags.js
@@ -1,6 +1,6 @@
 function getFlags(flag){
-    const index = process.argv.indexOf(flag) + 1
-    return process.argv[index]
+    const index = process.argv.indexOf(flag) + 1 //procurando o numero da flag dentro, pegando a posição mais 1 q é o index da frente
+    return process.argv[index]//pegando o resultado
 }
 
 module.exports = getFlags;
--- nodeJS/desafio/greetings.js
+++ nodeJS/desafio/greetings.js
@@ -1,1 +1,4 @@
-const getFlagValue = require('./flags')
+const getFlags = require('./flags')
+const getFlagValue = require('./flags')//importando 
+
+console.log(`Oi ${getFlags('--name')}. ${getFlags('--greeting')}`)
