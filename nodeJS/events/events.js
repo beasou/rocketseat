553566737ee207401a58bd5d8291cf559fa8d748
beasou <beatrizreginasouza1@gmail.com>
2022-02-03 15:34:30 -0300

    Once- ouvindo eventos uma unica vez

--- nodeJS/events/events.js
+++ nodeJS/events/events.js
@@ -2,8 +2,15 @@ const { EventEmitter } = require('events')
 const ev = new EventEmitter()
 // console.log(ev)
 
+/*
 //- ouvindo emissão de eventos
 ev.on('saySomething', (message) => {
+    console.log('Eu ouvi você', message)
+}) //execução de eventos
+*/
+
+//- ouvindo eventos uma unica vez
+ev.once('saySomething', (message) => {
     console.log('Eu ouvi você', message)
 }) //execução de eventos
 
