f9389a4367b8deffe36c979e92012d372e87c5b3
beasou <beatrizreginasouza1@gmail.com>
2022-02-03 17:21:23 -0300

    NodeJS + EJS / criando servidor

---
+++ nodeJS_EJS/server.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express() //estanciando o express e atribuindo a variavel app
+
+app.set("view engine", "ejs") // a ferramenta que eu vou utilizar para redenrizar o html e o ejs
+
+//get é um metodo que esta dentro do express
+app.get("/", function (req, res){ //aqui está a rota "/"->dominio sem nada na frente
+    res.render("index")//a resposta é renderizar o index res=response
+})
+
+app.get("/sobre", function (req, res){
+    res.render("about")
+})
+
+app.listen(8080) //função dentro do express que fica "ouvindo"
+console.log("Rodando")
