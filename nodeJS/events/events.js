2e226643b6dcaac9b4c099db078fd98730125559
beasou <beatrizreginasouza1@gmail.com>
2022-02-03 15:29:56 -0300

    ouvindo e executando açoes para eventos

--- nodeJS/events/events.js
+++ nodeJS/events/events.js
@@ -1,7 +1,13 @@
 const { EventEmitter } = require('events')
 const ev = new EventEmitter()
-
 // console.log(ev)
 
+//- ouvindo emissão de eventos
+ev.on('saySomething', (message) => {
+    console.log('Eu ouvi você', message)
+}) //execução de eventos
+
 //- emitindo evento
-ev.emit('saySomething')
+ev.emit('saySomething', 'Ana')
+ev.emit('saySomething', 'Bea')
+ev.emit('saySomething', 'Lia')
