4aadebb45a27e6bbe9dde51738ff5c470a5b36bf
beasou <beatrizreginasouza1@gmail.com>
2022-02-09 12:32:26 -0300

    programaçao funcional - imperativa vs declarativa

---
+++ programacaoFuncional/programacao.js
@@ -0,0 +1,15 @@
+// PROGRAMAÇÃO IMPERATIVA VS DECLARATIVA
+
+// Função que eleva o número ao quadrado
+// Imperativo: Faça da seguinte forma
+let number = 2
+
+function square(){
+    return number * number
+}
+
+number = square()
+
+
+// Declarativo: O que fazer e não como fazer.
+const square = n => n * n
