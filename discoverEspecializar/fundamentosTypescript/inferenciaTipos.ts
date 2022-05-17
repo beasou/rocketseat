6c28f3e7b90e56b6564ef5516c1d1e77f0ea9f44
beasou <beatrizreginasouza1@gmail.com>
2022-05-17 16:59:50 -0300

    Inferência de tipos

---
+++ discoverEspecializar/fundamentosTypescript/inferenciaTipos.ts
@@ -0,0 +1,8 @@
+//Inferência de tipos]
+    //reconhecer de forma automatica o tipo de variavel, baseado no conteudo atribuído na declaração
+
+let user: string = 'Beatriz' //posso retirar a declaração feita de forma explicita
+
+let useR = 'Beatriz'
+
+useR = 10 //erro, pois em useR esta inferido o tipo string
