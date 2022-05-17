3124ca866c677efd6c24f3c015ba8c4d65e2e7d1
beasou <beatrizreginasouza1@gmail.com>
2022-05-17 17:14:00 -0300

    Tipos Primitivos

---
+++ discoverEspecializar/fundamentosTypescript/tiposPrimitivos.ts
@@ -0,0 +1,18 @@
+//tipos basicos disponiveis 
+
+//boolean
+let loading: boolean
+loading = true //or false
+
+//string
+let email: string
+email = 'bea@bea.com' //tipo texto
+
+//number
+let price:number
+price = 10.50 //pode ser com casa decimais
+price = 10 //ou inteiros
+
+//ATRIBUIINDO UM VALOR DIFERENTE DO TIPO DECLARADO VAI DAR ERRO
+
+
