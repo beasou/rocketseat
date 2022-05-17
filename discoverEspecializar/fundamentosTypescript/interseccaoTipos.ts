47cb39d4f4e317f39fdae1d5ead4bd51b153bc61
beasou <beatrizreginasouza1@gmail.com>
2022-05-17 19:06:00 -0300

    Intersecção de Tipos

---
+++ discoverEspecializar/fundamentosTypescript/interseccaoTipos.ts
@@ -0,0 +1,33 @@
+//Intersecção de Tipos
+
+type User = {
+    id: number
+    name: string
+}
+
+type Char = {
+    nickname: string
+    level: number
+}
+
+type PlayerInfo = User & Char //& uniao de tipos 
+let info: PlayerInfo ={ //posso tirar um ou outro para testar que as propriedades do retirado não fica~]ao disponiveis 
+    //ctrl + "barra de espaço", vai aparecer tds as propriedades de user e char
+    id: 1,
+    name:'Nome Sobrenome',
+    nickname:'nomesobre',
+    level:10
+}
+
+
+///---- DA PARA FAZER ASSIM TBM
+type PlayerInfo1 = User & {
+    nickname:string,
+    level: number
+}
+let info1: PlayerInfo ={ 
+    id: 1,
+    name:'Nome Sobrenome',
+    nickname:'nomesobre',
+    level:10
+}
