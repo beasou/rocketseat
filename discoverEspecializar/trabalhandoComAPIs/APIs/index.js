ff0c4d4d67d57d7c79309a542ad0f5bfed0d557c
beasou <beatrizreginasouza1@gmail.com>
2022-05-19 18:10:44 -0300

    GET

--- discoverEspecializar/trabalhandoComAPIs/APIs/index.js
+++ discoverEspecializar/trabalhandoComAPIs/APIs/index.js
@@ -6,3 +6,8 @@ app.listen('3000') //usando o metodo listen do express para ouvir a porta 3000
 //executando o comando "node ." no terminal, quando o package.json tiver em main: index.js, se não tenho que rodar o node e nome do arquivo
 
 //posso pegar esse aquivo todo e jogar em um arquivo server tbm
+
+app.route('/').get((req, res) => res.send("Oi")) //função do express que trabalha com rotas, o primeiro arguimento é a rota q no caso é /, poderia ser /sobre. O metodo passado é o get e dentro de get eu informo o que vai acontecer o que vai ser mostrado, dentro dele é passado o req(requisição) e o res(resposta)
+
+//outra rota ex
+app.route('/sobre').get((req, res) => res.send("Rota sobre"))
