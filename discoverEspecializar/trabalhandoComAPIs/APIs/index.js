579dd09efee1695ccc54a52e923523866a96f740
beasou <beatrizreginasouza1@gmail.com>
2022-05-19 17:59:20 -0300

    Criando server

--- discoverEspecializar/trabalhandoComAPIs/APIs/index.js
+++ discoverEspecializar/trabalhandoComAPIs/APIs/index.js
@@ -4,3 +4,5 @@ const app = express() //inicializando express e guardando ele iniciado em app
 app.listen('3000') //usando o metodo listen do express para ouvir a porta 3000
 
 //executando o comando "node ." no terminal, quando o package.json tiver em main: index.js, se não tenho que rodar o node e nome do arquivo
+
+//posso pegar esse aquivo todo e jogar em um arquivo server tbm
