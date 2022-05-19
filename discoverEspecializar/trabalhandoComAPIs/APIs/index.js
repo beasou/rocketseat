c67d157dc19d43957963a4a8758b0024ef7509ec
beasou <beatrizreginasouza1@gmail.com>
2022-05-19 17:58:07 -0300

    Instalando Express

---
+++ discoverEspecializar/trabalhandoComAPIs/APIs/index.js
@@ -0,0 +1,6 @@
+const express = require('express') //chamando o metodo do express e guardando na constante
+const app = express() //inicializando express e guardando ele iniciado em app
+
+app.listen('3000') //usando o metodo listen do express para ouvir a porta 3000
+
+//executando o comando "node ." no terminal, quando o package.json tiver em main: index.js, se não tenho que rodar o node e nome do arquivo
