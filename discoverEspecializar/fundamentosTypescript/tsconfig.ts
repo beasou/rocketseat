1e064e5bedc66452e1acc2b0f0219e16e5866f73
beasou <beatrizreginasouza1@gmail.com>
2022-05-19 15:40:34 -0300

    tsconfig

---
+++ discoverEspecializar/fundamentosTypescript/tsconfig.ts
@@ -0,0 +1,8 @@
+//arquivo de configuração
+
+/*O arquivo tsconfig. json especifica os arquivos raiz e as
+configurações de compilação necessárias para o projeto. 
+Projetos JavaScript podem ter um arquivo jsconfig. json , 
+que tem quase o mesmo propósito, mas possui algumas 
+flags do compilador relacionadas ao JavaScript que já estão 
+habilitadas por padrão. */
