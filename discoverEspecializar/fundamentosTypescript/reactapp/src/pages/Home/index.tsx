2853fb68def617187d2155600e72a818c6ebcb68
beasou <beatrizreginasouza1@gmail.com>
2022-05-19 17:21:47 -0300

    Tipando resposta da API

--- discoverEspecializar/fundamentosTypescript/reactapp/src/pages/Home/index.tsx
+++ discoverEspecializar/fundamentosTypescript/reactapp/src/pages/Home/index.tsx
@@ -2,11 +2,22 @@ import React, {useState, useEffect} from 'react'
 import './style.css'
 import {Card, CardProps} from '../../components/Card'
               //importo a CardProps do Card
+
+type ProfileResponse = { //tipando resposta da API
+  name: string
+  avatar_url: string
+}
+
+type User ={ //for setUser()
+  name: string
+  avatar: string
+}
+
 export function Home() {
  //conceitos de estado 
   const [studentName, setstudentName] = useState('')
   const [students, setStudents] = useState<CardProps[]>([]) //utilizo o cardpross,com <>, como é um array add []
-  const [user,setUser] = useState({name:'', avatar:''})
+  const [user,setUser] = useState<User>({} as User) //eu informo q é do tipo User inicio como um obj vazio como(as) User, ao inves de inicializar com vazio name:'' e avatar:'', pois se tivesse 30 propriedades dentro de user iria ficar gigantesco
 
   function handleAddStudent(){ 
     const newStudent = { 
@@ -23,9 +34,9 @@ export function Home() {
   useEffect(() =>{
     async function fetchData(){
       const response = await fetch('https://api.github.com/users/beasou')
-      const data = await response.json()
-      console.log("Dados => ", data)
-      setUser({
+      const data = await response.json() as ProfileResponse
+      
+      setUser({ //pelo Profile Response não ser compativel com setUser, vamos fazer uma outra tipagem
         name:data.name,
         avatar: data.avatar_url,
       })
