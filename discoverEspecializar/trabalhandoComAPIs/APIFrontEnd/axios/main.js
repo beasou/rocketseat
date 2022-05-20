1b2700aec83481bb4fdc3bff7f7975d0e9ee3617
beasou <beatrizreginasouza1@gmail.com>
2022-05-20 18:53:50 -0300

    Api com axios - put

--- discoverEspecializar/trabalhandoComAPIs/APIFrontEnd/axios/main.js
+++ discoverEspecializar/trabalhandoComAPIs/APIFrontEnd/axios/main.js
@@ -29,12 +29,25 @@ function getUser(id){
     .catch(error => console.error(error))
 }
 
+function updateUser(id, userUpdated){
+    axios.put(`${url}/${id}`, userUpdated)
+    .then(response => console.log(response))
+    .catch(error => console.error(error))
+}
+
 const newUser = {
     name: "Bea Sou",
     avatar: "https://avatars.githubusercontent.com/u/8356862?v=4",
     city: "Rio de Janeiro "
 }
 
+const userUpdated = {
+    name: "Bea Sou",
+    avatar: "https://avatars.githubusercontent.com/u/8356862?v=4",
+    city: "Rio de Janeiro "
+}
+
+updateUser(2, userUpdated)
 getUser(2)
 //addNewUser(newUser)
 getUsers()
