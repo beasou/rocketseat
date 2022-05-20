7bb39e5cfb3efb6859f1f154ae88154f25a04ffc
beasou <beatrizreginasouza1@gmail.com>
2022-05-19 23:47:44 -0300

    Consumindo API do GitHub  com axios

---
+++ discoverEspecializar/trabalhandoComAPIs/apiGithub/index.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const axios = require('axios')
+const app = express()
+
+app.listen('3000')
+app.route('/').get((req, res) =>{
+    axios.get('https://api.github.com/users/beasou')
+    .then(result => res.send(`<img src="${result.data.avatar_url}" />`))
+    .catch(error => console.log(error))
+})
