90bbf7ddaa33ebe22bbe03b5adea9d332cdda148
beasou <beatrizreginasouza1@gmail.com>
2022-05-24 13:51:18 -0300

    Fast Refresh no Webpack

--- ignite/reactjs/01-githubExplorer/webpack.config.js
+++ ignite/reactjs/01-githubExplorer/webpack.config.js
@@ -1,5 +1,7 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin')
+
 const isDevelopment = process.env.NODE_ENV !== 'production'
 
 module.exports ={
@@ -16,18 +18,27 @@ module.exports ={
     },
     devServer: {
         static: path.resolve(__dirname, 'public'),
+        hot: true,
     },
     plugins:[
+        isDevelopment &&  new ReactRefreshWebpackPlugin(),//condicional
         new HtmlWebpackPlugin({
             template: path.resolve(__dirname,'public','index.html') //qual arquivo de templete q ele vai utilizar para gerar o html, que é o index.html
         })
-    ],
+    ].filter(Boolean),//remove o false pra poder usar condicional acima
     module:{
         rules: [
             {
                 test: /\.jsx$/, //recebe uma expressao regular para conf se é um arquivo js ou não
                 exclude:/node_modules/,
-                use: 'babel-loader', //converter usando babel
+                use: {
+                    loader: 'babel-loader',//converter usando babel
+                    options: {
+                        plugins: [
+                            isDevelopment && require.resolve('react-refresh/babel')
+                        ].filter(Boolean)
+                    }
+                }, 
             },
             {
                 test: /\.scss$/, //recebe uma expressao regular para conf se é um arquivo css ou não
