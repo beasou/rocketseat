1d76ee256bddad803cd8c7385e764117494ce801
beasou <beatrizreginasouza1@gmail.com>
2022-05-24 14:32:12 -0300

    Utilizando o useEffect

--- ignite/reactjs/01-githubExplorer/src/components/RepositoryList.jsx
+++ ignite/reactjs/01-githubExplorer/src/components/RepositoryList.jsx
@@ -1,6 +1,9 @@
-import { RepositoryItem } from "./RepositoryItem";
+import { useState, useEffect } from 'react'
+import { RepositoryItem } from "./RepositoryItem"
 
 import '../styles/repositories.scss'
+
+const apiGitHub = "https://api.github.com/users/beasou/repos"
 
 const repository = {
     name: 'unform',
@@ -9,6 +12,14 @@ const repository = {
 }
 
 export function RepositoryList(){
+    const [repositories, setRepositories] = useState([])
+    
+    useEffect(() => {
+        fetch(apiGitHub)
+        .then(response => response.json())
+        .then(data => setRepositories(data))
+    }, [])
+
     return(
         <section className="repositoryList">
             <h1>Lista de Repositórios</h1>
