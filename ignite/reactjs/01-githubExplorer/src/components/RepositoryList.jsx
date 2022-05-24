595b8715f3154af195dd41e340daa066ce395586
beasou <beatrizreginasouza1@gmail.com>
2022-05-24 11:59:47 -0300

    Primeiro componente React

---
+++ ignite/reactjs/01-githubExplorer/src/components/RepositoryList.jsx
@@ -0,0 +1,36 @@
+const repositoryName = 'unform';
+
+export function RepositoryList(){
+    return(
+        <section className="repositoryList">
+            <h1>Lista de Repositórios</h1>
+            <ul>
+                <li>
+                    <strong>{repositoryName}</strong>
+                    <p>Forms in React</p>
+                    <a href="">Acessar repositório</a>
+                </li>
+                <li>
+                    <strong>unform</strong>
+                    <p>Forms in React</p>
+                    <a href="">Acessar repositório</a>
+                </li>
+                <li>
+                    <strong>unform</strong>
+                    <p>Forms in React</p>
+                    <a href="">Acessar repositório</a>
+                </li>
+                <li>
+                    <strong>unform</strong>
+                    <p>Forms in React</p>
+                    <a href="">Acessar repositório</a>
+                </li>
+                <li>
+                    <strong>unform</strong>
+                    <p>Forms in React</p>
+                    <a href="">Acessar repositório</a>
+                </li>
+            </ul>
+        </section>
+    );
+}
