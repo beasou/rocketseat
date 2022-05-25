d9ce2aa399898ac5079ecc73d660c732015d3b68
beasou <beatrizreginasouza1@gmail.com>
2022-05-25 13:53:16 -0300

    Componentes com TypeScript

--- ignite/reactjs/01-githubExplorer/src/components/RepositoryList.tsx
+++ ignite/reactjs/01-githubExplorer/src/components/RepositoryList.tsx
@@ -1,12 +1,17 @@
 import { useState, useEffect } from 'react'
 import { RepositoryItem } from "./RepositoryItem"
-
 import '../styles/repositories.scss'
 
 const apiGitHub = "https://api.github.com/users/beasou/repos"
 
+interface Repository{
+    name: string
+    description: string
+    html_url: string
+}
+
 export function RepositoryList(){
-    const [repositories, setRepositories] = useState([])
+    const [repositories, setRepositories] = useState<Repository[]>([]) //<> => generic, <o tipo q eu qro armazenar>, no caso um repositório, se eu colocar apenas <Repository>, irá armazenar o repositório, como o repositório é uma lista de repositório eu adiciono [] 
     
     useEffect(() => {
         fetch(apiGitHub)
